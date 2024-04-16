c71479f5021ff349a594f88f35a077503835ae9a
harryisawesome123 <harish.rahukulan25@ycdsbk12.ca>
2024-04-16 12:54:36 -0400

    commit

--- Sketch.java
+++ Sketch.java
@@ -1,17 +1,32 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Sketch extends PApplet {
+  PImage imgMeteor;
+
+  // x and y coordinates for the meteor
+  float fltMeteorX = 0;
+  float fltMeteorY = 0;
+
+  // meteor speed varibles
+  float fltXSpeed = 3;
+  float fltYSpeed = 5;
+
+  // define colours
+  int Black = color(0);
+  int While = color(255);
+  int Magenta = color(255, 0, 255);
 
   public void settings() {
     size(400, 400);
   }
 
   public void setup() {
-    background(0, 0, 0);
+    background(Black);
     // load meteor
-
+    imgMeteor = loadImage("spaceMeteors_003.png");
     // resize meteor
-    
+    imgMeteor.resize(50, 50);
     // load missile
 
     // resize missile
@@ -24,8 +39,21 @@ public class Sketch extends PApplet {
 
   public void draw() {
     // draw meteor and move
+    background(Black);
 
+    // draw meteor and postion
+    image(imgMeteor, fltMeteorX, fltMeteorY);
 
+    fltMeteorX += fltXSpeed;
+    fltMeteorY += fltYSpeed;
+
+    if (fltMeteorX > width - imgMeteor.width || fltMeteorX < 0) {
+      fltXSpeed *= -1;
+      
+    }
+    if (fltMeteorY > height - imgMeteor.height || fltMeteorY < 0) {
+      fltYSpeed *= -1;
+    }
     // draw missile and move
 
 
